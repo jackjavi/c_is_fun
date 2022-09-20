fb4102afc454c3d0ab24edcecb4bf7d3f39cff06
jackjavi <jackmtembete@gmail.com>
2022-09-20 05:36:04 -0700

    print the size of a double

---
+++ 0-size_double.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * size_double - prints the size of a double
+ *
+ * Return: Nothing.
+ */
+void size_double(void)
+{
+	printf("Size of a double is: %lu\n", sizeof(double));
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void size_double(void);
+
+#endif /* MAIN_H */
