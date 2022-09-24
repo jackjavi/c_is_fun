869562ab0dec0784b99bff3c917b7ca003de7d26
jackjavi <jackmtembete@gmail.com>
2022-09-24 13:12:45 -0700

    updated: floats

--- 4-main.c
+++ 4-main.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;
@@ -16,8 +16,8 @@ int main(void)
 	fahr = lower;
 	while (fahr <= upper)
 	{
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0 / 9.0) * (fahr - 32);
+		printf("%3.0f%6.1f\n", fahr, celsius);
 		fahr += step;
 	}
 
