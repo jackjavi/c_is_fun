2174aaa95b41fd38c9ab16cdc851cf2bffa2d07d
jackjavi <jackmtembete@gmail.com>
2022-10-10 07:10:35 -0700

    macros

---
+++ 22-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * macro definition
+ */
+#define LIMIT 5
+
+/**
+ * main - introduction to macros
+ * Return: Always (0);
+ */
+int main(void)
+{
+	printf("The value of limit is: %d\n", LIMIT);
+
+	return (0);
+}
